1c617d382f93367e0a3461f1f591c55560be1445
Yash <yashhh2004@gmail.com>
2025-01-26 05:08:22 +0530

    Test v10

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(cors({ origin: 'https://hackathon-sand-six.vercel.app/' }));
+app.use(cors({ origin: 'https://hackathon-sand-six.vercel.app' }));
 const API_KEY = "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";
 const OPENAI_CHAT_API_URL = "https://api.openai.com/v1/chat/completions";
 const OPENAI_IMAGE_API_URL = "https://api.openai.com/v1/images/generations";
@@ -29,9 +29,9 @@ app.post("/chat", async (req, res) => {
       {
         model: "gpt-4",
         messages: [{ role: "user", content: prompt }],
-        max_tokens: 1024, // Increase token limit for longer responses
-        temperature: 0.7,  // Balance between creativity and focus
-        top_p: 1,          // Use full token distribution for creative responses
+        max_tokens: 4096, // Increase token limit for longer responses
+        temperature: 0.1,  // Balance between creativity and focus
+        top_p: 0.1,          // Use full token distribution for creative responses
         frequency_penalty: 0, // No penalty for repeated phrases
         presence_penalty: 0,  // Neutral presence for repeated topics
       },
